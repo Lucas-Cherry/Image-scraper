372e0cf863869ed2a242842fc8716063cf0205fc
Lukasz Wisniewski <lukaszwisniewski1236@gmail.com>
2023-03-08 15:26:15 +0100

    1.1

--- Source.cpp
+++ Source.cpp
@@ -17,9 +17,16 @@ size_t callbackfunction(void* ptr, size_t size, size_t nmemb, void* userdata)
     return written;
 }
 
-bool download_jpeg(char* url)
+char* to_char_arr(std::string s) {
+    const int length = s.length();
+    char* char_arr = new char[length + 1];
+    strcpy(char_arr, s.c_str());
+    return char_arr;
+}
+
+bool download_image(char* url,std::string file_name)
 {
-    FILE* fp = fopen("out.png", "wb");
+    FILE* fp = fopen(to_char_arr(file_name), "wb");
     if (!fp)
     {
         printf("!!! Failed to create file on the disk\n");
@@ -54,29 +61,68 @@ bool download_jpeg(char* url)
     return true;
 }
 
-//std::string extract_html() { 
-//    cpr::Url adres = cpr::Url{ "" };
-//    cpr::Response odpowiedz = cpr::Get(adres);
-//    return odpowiedz.text;
-//}
+std::string extract_html(std::string s) { 
+    cpr::Url adres = cpr::Url{s};
+    cpr::Response odpowiedz = cpr::Get(adres);
+    return odpowiedz.text;
+}
+
+
+void find_images(GumboNode * node, std::vector<std::string> &image_sources) {
+    if (node->type != GUMBO_NODE_ELEMENT) return;
+
+    if (node->v.element.tag == GUMBO_TAG_A) {
+
+        GumboAttribute* href = gumbo_get_attribute(&node->v.element.attributes, "href"); 
+
+        if (href) {
+            std::string link = href->value;
+            if (link.rfind(".png") != std::string::npos || link.rfind(".jpg") != std::string::npos) {
+                link = "https:" + link;
+                if (std::find(image_sources.begin(), image_sources.end(), link) == image_sources.end()) {
+                    image_sources.push_back(link);
+                }
+            }
+        }
+        return;
+    }
+
+
+    GumboVector* children = &node->v.element.children;
+    for (unsigned int i = 0; i < children->length; i++) {
+        find_images(static_cast<GumboNode*>(children->data[i]),image_sources);
+    }
+}
+
+void download_images(std::vector<std::string> img_sources) {
+    for (int i = 0; i < img_sources.size() ; i++) {
+        std::string file_name = "C:\\Users\\£ukasz\\Desktop\\imgs\\img"+std::to_string(i)+".png";
+        char* url = to_char_arr(img_sources[i]);
+
+        if (!download_image(&url[0],file_name)) {
+            printf("!! Failed to download file: %s\n", &url[0]);
+            return;
+        }
+
+        delete[] url;
+    }
+}
+
 
 int main(){
 
-    std::string s = ""; // site url goes here
 
-    const int length = s.length();
+    std::string content = extract_html("https://boards.4channel.org/a/thread/249823792");
 
-    char* url = new char[length + 1];
+    GumboOutput* content_gumbo = gumbo_parse(content.c_str());
 
-    strcpy(url, s.c_str());
+    std::vector<std::string> image_sources;
 
-    if (!download_jpeg(&url[0]))
-    {
-        printf("!! Failed to download file: %s\n", &url[0]);
-        return -1;
-    }
+    find_images(content_gumbo->root,image_sources);
 
-    delete[] url;
+    download_images(image_sources);
+
+    gumbo_destroy_output(&kGumboDefaultOptions, content_gumbo);
 
     return 0;
 }
